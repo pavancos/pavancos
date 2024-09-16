8cf4a38a3ef6f3e4e6843db52c4cc516650038af
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-09-16 22:55:57 +0530

    init: project

---
+++ src/components/Contactcard/ContactCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ContactCard = () => {
+  return (
+    <div className='w-10/12 h-1/2 sm:w-1/2 sm:h-1/2 bg-white p-4 rounded-sm shadow-md mb-32'>
+      <div className="h-full w-full border border-1 border-slate-800 rounded-sm flex flex-col justify-center">
+        <h1 className='text-2xl'>
+            Pavan Kumar Chennupati
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default ContactCard
